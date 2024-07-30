06d87391a39da1f8472667be6b4c8b0c4be2134e
vtran-at-speer <viet.tran@speer.io>
2024-07-29 21:57:04 -0400

    Added react router dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types='vitest' />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
