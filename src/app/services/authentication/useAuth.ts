045c4e0bfe3a9d82346c9bd03234a2571bd9f615
Viet Tran <70352144+tienviet10@users.noreply.github.com>
2024-09-07 16:51:17 -0400

    [GLF-4] Prepare API calls (#5)
    * feat: add signin api call
    
    * feat: memoize return value
    
    * Delete .env
    
    * feat: add endpoints in constants
    
    * feat: add get user info hook
    
    * feat: add game filter hook

---
+++ src/app/services/authentication/useAuth.ts
@@ -0,0 +1,74 @@
+import { useMutation } from '@tanstack/react-query';
+import client from '@app/utils/authApi';
+import { CustomAxiosResponse, ErrorResponse, UserData } from '@app/constants/global/types';
+import { loginRoute, registerRoute } from '@app/constants/global/urls';
+import { useCallback, useMemo } from 'react';
+
+type LoginParams = {
+  email: string;
+  password: string;
+};
+
+type LoginResponse = {
+  token: string;
+  user: UserData;
+};
+
+type SignUpParams = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export const useAuth = () => {
+  const loginUser = useCallback(
+    async (params: LoginParams): Promise<CustomAxiosResponse<LoginResponse>> => client.post(loginRoute, params),
+    []
+  );
+
+  const signUpUser = useCallback(
+    async (params: SignUpParams): Promise<CustomAxiosResponse<LoginResponse>> => client.post(registerRoute, params),
+    []
+  );
+
+  const {
+    mutate: signInMutation,
+    data: signInResponse,
+    error: signInError,
+    isError: signInIsError,
+  } = useMutation<CustomAxiosResponse<LoginResponse>, ErrorResponse, LoginParams>({
+    mutationFn: loginUser,
+  });
+
+  const {
+    mutate: signUpMutation,
+    data: signUpResponse,
+    error: signUpError,
+    isError: signUpIsError,
+  } = useMutation<CustomAxiosResponse<LoginResponse>, ErrorResponse, SignUpParams>({ mutationFn: signUpUser });
+
+  const memoizedValue = useMemo(
+    () => ({
+      signInMutation,
+      signInResponse,
+      signInError,
+      signInIsError,
+      signUpMutation,
+      signUpResponse,
+      signUpError,
+      signUpIsError,
+    }),
+    [
+      signInMutation,
+      signInResponse,
+      signInError,
+      signInIsError,
+      signUpMutation,
+      signUpResponse,
+      signUpError,
+      signUpIsError,
+    ]
+  );
+
+  return memoizedValue;
+};
---
+++ src/app/services/authentication/useGetUser.ts
@@ -0,0 +1,29 @@
+import { CustomAxiosResponse, ErrorResponse, UserData } from '@app/constants/global/types';
+import { userInfoRoute } from '@app/constants/global/urls';
+import client from '@app/utils/authApi';
+import { useQuery } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
+
+const useGetUser = () => {
+  const getUser = useCallback(async (): Promise<CustomAxiosResponse<UserData>> => client.get(userInfoRoute), []);
+
+  const {
+    data: userInfo,
+    error: userDataError,
+    isLoading: userDataIsLoading,
+    refetch: getUserData,
+  } = useQuery<CustomAxiosResponse<UserData>, ErrorResponse>({
+    queryKey: ['user'],
+    queryFn: getUser,
+    enabled: false,
+  });
+
+  const memoizedReturnedValues = useMemo(
+    () => ({ userInfo, userDataIsLoading, getUserData, userDataError }),
+    [userInfo, userDataIsLoading, getUserData, userDataError]
+  );
+
+  return memoizedReturnedValues;
+};
+
+export default useGetUser;
