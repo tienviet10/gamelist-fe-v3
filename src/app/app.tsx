2db9b594ecf8e48aa17a8da72239d523fffe8b5e
Clement <clement.shum128@gmail.com>
2024-09-07 09:11:48 -0400

    test screens

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import NxWelcome from '@app/nx-welcome';
+import Login from '@app/components/login/Login';
+import SignUp from './components/signup/SignUp';
 
 export function App() {
   return (
     <div>
+      <Login />
+      <SignUp />
       <NxWelcome title="gamelist-fe" />
     </div>
   );
