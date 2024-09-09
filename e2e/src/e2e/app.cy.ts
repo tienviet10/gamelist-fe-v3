6bcdae0180fb7d81e05f3261e9930054410fe653
tienviet10 <viettran101294@gmail.com>
2024-09-08 21:59:54 -0400

    feat: add eslint exception to cypress files

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
--- e2e/src/support/app.po.ts
+++ e2e/src/support/app.po.ts
@@ -1,3 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 
 export const getGreeting = () => cy.get('h1');
