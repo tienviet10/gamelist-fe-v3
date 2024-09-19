4426e31d77ad637990cde50896bf1adff22ea729
Clement <clement.shum128@gmail.com>
2024-09-18 22:30:49 -0400

    update formSchema to use checks instead of useState

--- src/app/components/SignUp/SignUp.tsx
+++ src/app/components/SignUp/SignUp.tsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-import { toast, ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { z } from 'zod';
+import { Link } from 'react-router-dom';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@lib/Button/Button';
 import { Checkbox } from '@lib/Checkbox/Checkbox';
-import { Form, FormControl, FormField, FormItem, FormMessage } from '@lib/Form/Form';
+import { Form, FormControl, FormField, FormItem, FormMessage, FormDescription } from '@lib/Form/Form';
 import { Input } from '@lib/Input/Input';
 
-import 'react-toastify/dist/ReactToastify.css';
-
-const formSchema = z.object({
-  email: z.string().min(2, { message: 'Please enter your email address' }).max(50),
-  username: z.string().min(2, { message: 'Name must be atleast 2 characters' }).max(50),
-  password: z.string().min(2, { message: 'Password be atleast 2 characters' }).max(50),
-  confirmPassword: z.string().min(2, { message: 'Confirm password must be atleast 2 characters' }).max(50),
-});
+const formSchema = z
+  .object({
+    email: z.string().min(2, { message: 'Email address missing' }).max(50),
+    username: z.string().min(2, { message: 'Please enter name' }).max(50),
+    password: z.string().min(2, { message: 'Please enter password' }).max(50),
+    confirmPassword: z.string().min(2, { message: 'Please confirm password' }).max(50),
+    termsOfService: z.boolean().refine((val) => val === true, {
+      message: 'Please accept the terms of service',
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords does not match',
+  });
 
 function SignUp() {
-  const [formCheckbox, setFormCheckbox] = useState(false);
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -28,24 +33,16 @@ function SignUp() {
       username: '',
       password: '',
       confirmPassword: '',
+      termsOfService: false,
     },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    if (formCheckbox === false) {
-      // check user agree to terms and conditions
-      toast.error('Please agree to our terms of service');
-    } else if (values.password !== values.confirmPassword) {
-      // check passwords match
-      toast.error('Passwords do not match');
-    } else {
-      // console.log(values);
-    }
+    // console.log(values);
   }
 
   return (
     <div className="mt-[60px] flex max-w-[400px] flex-col items-center rounded-md bg-[#fff] p-10">
-      <ToastContainer />
       <Form {...form}>
         <div className="mb-[60px] mt-[20px] text-[24px] font-semibold">
           <p>Sign up to GameList</p>
@@ -108,18 +105,21 @@ function SignUp() {
           />
 
           {/* Checkbox */}
-          <div className="flex flex-row items-center justify-center text-[14px] text-[#8f99a1]">
-            <Checkbox
-              checked={formCheckbox}
-              className="border-inherit"
-              onClick={() => {
-                setFormCheckbox(!formCheckbox);
-              }}
-            />
-            <p>&nbsp;You agree to our terms of service</p>
-          </div>
-
-          {/* {modal && <p className="text-[14px] text-[rgb(237,62,62)]">Please agree to our terms of service</p>} */}
+          <FormField
+            control={form.control}
+            name="termsOfService"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-row items-center text-[14px] text-[#8f99a1]">
+                  <FormControl>
+                    <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                  </FormControl>
+                  <p className="mt-0">&nbsp;You agree to our terms of service</p>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
           <Button size="loginSignUp" type="submit" variant="loginSignUp">
             Sign up
@@ -128,13 +128,13 @@ function SignUp() {
       </Form>
 
       <div className="mt-[80px] flex flex-row text-[14px]">
-        <a className="flex flex-row text-[#8f99a1] hover:text-[#3db4f2]" href="/login">
+        <Link to="/login" className="flex flex-row text-[#8f99a1] hover:text-[#3db4f2]">
           Login
-        </a>
+        </Link>
         <p>&nbsp;•&nbsp;</p>
-        <a className="flex flex-row text-[#8f99a1] hover:text-[#3db4f2]" href="/resend-verification-email">
+        <Link to="/resend-verification-email" className="flex flex-row text-[#8f99a1] hover:text-[#3db4f2]">
           Resend Verification Email
-        </a>
+        </Link>
       </div>
     </div>
   );
