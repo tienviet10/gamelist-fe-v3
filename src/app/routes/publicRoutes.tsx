b88dd7cac118d2084d412a0c3ac8440f7a1870d0
tienviet10 <viettran101294@gmail.com>
2025-01-30 23:40:53 -0500

    fix: refactor game detail link

--- src/app/routes/publicRoutes.tsx
+++ src/app/routes/publicRoutes.tsx
@@ -1,5 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 
+import GameDetail from '@app/pages/GameDetail/GameDetail';
 import HomePage from '@app/pages/Home/HomePage';
 import LoginPage from '@app/pages/Login/LoginPage';
 import SignUpPage from '@app/pages/SignUpPage';
@@ -9,4 +10,13 @@ export const publicRoutes: RouteObject[] = [
   { path: '/games', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/signup', element: <SignUpPage /> },
+  {
+    path: '/game-detail',
+    children: [
+      {
+        path: '/game-detail/:id/:name',
+        element: <GameDetail />,
+      },
+    ],
+  },
 ];
